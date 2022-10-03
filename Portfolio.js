4b04260706f7e7a8eb88d29e6360e6f06137ef77
Sai Hruthik Reddy Varakantham <105242080+VarakanthamSaiHruthikReddy975@users.noreply.github.com>
2022-10-02 21:29:15 -0500

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,1 +1,7 @@
 window.alert("Hello Everyone, Thank You for clicking on my website, For more please click ok ");
+let firstname
+document.getElementById("submitbutton").onclick=function()
+{
+    firstname = document.getElementById("fname").value;
+    window.alert("Hello "+firstname+" ,Thank You for sending your details, will reach out to you as soon possible");
+}
